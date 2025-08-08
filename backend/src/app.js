9af49c91c58cf61ed15b4b06582dc6eb87652db3
Tarique <mdtariqueh1@gmail.com>
2025-08-08 12:42:56 +0530

    fix: update CORS origin to production URL

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173", // your frontend URL
+    origin: "https://trqoder-auth.netlify.app", // your frontend URL
     credentials: true,
   })
 );
