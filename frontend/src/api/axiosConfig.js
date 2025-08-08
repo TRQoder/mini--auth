ce0309793f357ca46c5cef6918aaadf6503cc992
Tarique <mdtariqueh1@gmail.com>
2025-08-08 12:38:03 +0530

    fix: update axios baseURL to production endpoint

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL : "https://mini-auth.onrender.com/api/auth/"
-  baseURL: "http://localhost:3000/api/auth",
+  baseURL: "https://mini-auth.onrender.com/api/auth/",
+  //   baseURL: "http://localhost:3000/api/auth",
   withCredentials: true, // ✅ Send cookies with every request
 });
 
